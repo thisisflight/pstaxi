45acd2c90dec6977b233542226adf186c7dec0d4
Andrey <varioustemp@gmail.com>
2021-12-24 20:02:05 +0300

    admin panel filters added

--- motorpool/admin.py
+++ motorpool/admin.py
@@ -1,7 +1,56 @@
 from django.contrib import admin
-from .models import Brand
+from .models import Brand, Auto, Option, VehiclePassport
+
+
+class EnginePowerFilter(admin.SimpleListFilter):
+    title = 'Мощность двигателя'
+    parameter_name = 'engine_power_filter'
+
+    def lookups(self, request, model_admin):
+        filter_list = [
+            ('0', '0-100'),
+            ('1', '101-200'),
+            ('2', '201-300')
+        ]
+        return filter_list
+
+    def queryset(self, request, queryset):
+        filter_value = self.value()
+        if filter_value == '0':
+            return queryset.filter(pts__engine_power__gte=0, pts__engine_power__lte=100)
+        elif filter_value == '1':
+            return queryset.filter(pts__engine_power__gte=101, pts__engine_power__lte=200)
+        elif filter_value == '2':
+            return queryset.filter(pts__engine_power__gte=201, pts__engine_power__lte=300)
+        return queryset
+
+
+class AutoInstanceInline(admin.TabularInline):
+    model = Auto
+    extra = 0
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
+    inlines = [AutoInstanceInline]
+
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number', 'brand', 'year', 'auto_class', 'display_engine_power']
+    list_select_related = ['brand', 'pts']
+    list_display_links = ['id', 'number', 'brand', ]
+    list_filter = [EnginePowerFilter, 'auto_class', 'options', ]
+    search_fields = ['brand__title', 'number', ]
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(VehiclePassport)
+class VehiclePassportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'auto', 'vin', 'engine_volume', 'engine_power']
+    list_filter = ['auto__brand', ]
+    list_select_related = ['auto__brand', ]
--- motorpool/models.py
+++ motorpool/models.py
@@ -1,12 +1,72 @@
 from django.db import models
+from utils.models import generate_unique_slug
 
 
 class Brand(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Бренд')
-
-    class Meta:
-        verbose_name = 'Бренд'
-        verbose_name_plural = 'Бренды'
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=210, default='', blank=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = generate_unique_slug(Brand, self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Бренды'
+
+
+class Option(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Опции'
+
+
+class Auto(models.Model):
+
+    AUTO_CLASS_ECONOMY = 'e'
+    AUTO_CLASS_COMFORT = 'c'
+    AUTO_CLASS_BUSINESS = 'b'
+
+    AUTO_CLASS_CHOICES = (
+        (AUTO_CLASS_ECONOMY, 'economy'),
+        (AUTO_CLASS_COMFORT, 'comfort'),
+        (AUTO_CLASS_BUSINESS, 'business'),
+    )
+
+    brand = models.ForeignKey(Brand, null=True, on_delete=models.CASCADE, related_name='cars')
+    options = models.ManyToManyField(Option, related_name='cars')
+    number = models.CharField(max_length=15)
+    description = models.TextField(max_length=2, default='', blank=True)
+    year = models.SmallIntegerField(null=True)
+    auto_class = models.CharField(max_length=1, null=True, choices=AUTO_CLASS_CHOICES, default=AUTO_CLASS_ECONOMY)
+
+    def __str__(self):
+        return self.number
+
+    def display_engine_power(self):
+        return self.pts.engine_power
+
+    display_engine_power.short_description = 'Engine power'
+
+    class Meta:
+        verbose_name_plural = 'Автомобили'
+
+
+class VehiclePassport(models.Model):
+    auto = models.OneToOneField(Auto, on_delete=models.CASCADE,
+                                related_name='pts', verbose_name=Auto._meta.verbose_name)
+    vin = models.CharField(max_length=30, verbose_name='Идентификационный номер (VIN)')
+    engine_volume = models.SmallIntegerField(verbose_name='Объем двигателя, куб.см')
+    engine_power = models.SmallIntegerField(verbose_name='Мощность двигателя, л.с.')
+
+    def __str__(self):
+        return f'{self.auto}::{self.vin}'
+
+    class Meta:
+        verbose_name_plural = 'Паспорта машин'
